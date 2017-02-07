62494f6119ae7faea9a2a7e8be3882b996450cbb
Wytse Jan Posthumus <wytse.jan.posthumus@topicus.nl>
2017-02-07 22:19:09 +0100

    initial commit

---
+++ AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PMSN-IM")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("Bahtinov_grabber")]
+[assembly: AssemblyCopyright("Copyright © PMSN-IM 2009")]
+[assembly: AssemblyTitle("Bahtinov_grabber")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a5130b05-342c-49e1-835e-46db01a75f64")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.1")]
